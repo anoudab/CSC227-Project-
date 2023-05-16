67b18090ca79240ec83d60a9547086a595719429
Alanood <111198226+anoudab@users.noreply.github.com>
2023-05-16 22:06:01 +0300

    Add files via upload

---
+++ Partition.java
@@ -0,0 +1,54 @@
+public class Partition
+{
+
+String partitionStatus;
+int partitionSize;
+int startingAddress;
+int endingAddress;
+String processId;
+int internalFrag;
+
+
+public Partition()
+{
+
+}
+
+public void Allocate()
+{
+
+}
+
+
+public void Deallocate()
+{
+
+
+
+}
+
+public void Report()
+{
+
+}
+
+public void WorstFit()
+{
+
+}
+
+public void BestFit()
+{
+
+}
+
+public void FirstFit()
+{
+
+}
+
+
+
+
+
+}
